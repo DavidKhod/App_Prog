fd70ded2bea136722c103fb23f4874e987d540ae
DavidKhod <Davidk207@gmail.com>
2023-11-20 22:33:17 +0200

    First commit

---
+++ MakingButtonDoSomething/MakingButtonDoSomething/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace MakingButtonDoSomething
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button plus;
+        TextView txtview1;
+        int cnt = 0;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            plus = FindViewById<Button>(Resource.Id.button1);
+            txtview1 = FindViewById<TextView>(Resource.Id.txtv1);
+            plus.Click += plus_Click;
+        }
+
+        private void plus_Click(object sender, System.EventArgs e)
+        {
+            txtview1.Text = cnt.ToString();
+            cnt++;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+}
