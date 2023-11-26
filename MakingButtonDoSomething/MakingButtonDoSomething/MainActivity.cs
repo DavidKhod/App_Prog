7a6933804fb37d51baf9f6d335b916f8633a412a
David_KH <Davidkh207@gmail.com>
2023-11-27 00:35:50 +0200

    Added the plus and minus buttons but with minor errors, hoping the teacher will help me

--- MakingButtonDoSomething/MakingButtonDoSomething/MainActivity.cs
+++ MakingButtonDoSomething/MakingButtonDoSomething/MainActivity.cs
@@ -9,25 +9,62 @@ namespace MakingButtonDoSomething
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button on;
+        Button off;
         Button plus;
+        Button min;
         TextView txtview1;
         int cnt = 0;
+        bool onOff = false;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            plus = FindViewById<Button>(Resource.Id.button1);
+            off = FindViewById<Button>(Resource.Id.off);
+            on = FindViewById<Button>(Resource.Id.on);
+            plus = FindViewById<Button>(Resource.Id.up);
+            min = FindViewById<Button>(Resource.Id.less);
+            
+
             txtview1 = FindViewById<TextView>(Resource.Id.txtv1);
-            plus.Click += plus_Click;
+            on.Click += On_Click;
+            off.Click += Off_Click;
+            plus.Click += Plus_Click;
+            min.Click += Min_Click;
         }
 
-        private void plus_Click(object sender, System.EventArgs e)
+        private void Min_Click(object sender, System.EventArgs e)
         {
-            txtview1.Text = cnt.ToString();
-            cnt++;
+            if (onOff)
+            {
+                cnt--;
+                txtview1.Text = cnt.ToString();
+            }
         }
+
+        private void Plus_Click(object sender, System.EventArgs e)
+        {
+            if (onOff)
+            {
+                cnt++;
+                txtview1.Text = cnt.ToString();
+            }
+        }
+
+        private void Off_Click(object sender, System.EventArgs e)
+        {
+            onOff = false;
+            txtview1.Text = cnt.ToString();
+        }
+
+        private void On_Click(object sender, System.EventArgs e)
+        {
+            onOff = true;
+            txtview1.Text = cnt.ToString();
+        }
+
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
