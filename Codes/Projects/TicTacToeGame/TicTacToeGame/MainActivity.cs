1ff5a6c93155dea7358028d335c7ea435b96f3b5
DavidKhod <Davidk207@gmail.com>
2024-01-08 20:22:45 +0200

    Fixed TicTacToe.cs function IfWon,changed the Array to a Matrix

--- Codes/Projects/TicTacToeGame/TicTacToeGame/MainActivity.cs
+++ Codes/Projects/TicTacToeGame/TicTacToeGame/MainActivity.cs
@@ -27,6 +27,102 @@ namespace TicTacToeGame
             SetContentView(Resource.Layout.activity_main);
             Init();
             topLeft.Click += TopLeft_Click;
+            top.Click += Top_Click;
+            topRight.Click += TopRight_Click;
+            middleLeft.Click += MiddleLeft_Click;
+            middle.Click += Middle_Click;
+            middleRight.Click += MiddleRight_Click;
+            bottomLeft.Click += BottomLeft_Click;
+            bottom.Click += Bottom_Click;
+            bottomRight.Click += BottomRight_Click;
+        }
+
+        private void BottomRight_Click(object sender, System.EventArgs e)
+        {
+            if (bottomRight.Text == " ")
+            {
+                bottomRight.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void Bottom_Click(object sender, System.EventArgs e)
+        {
+            if (bottom.Text == " ")
+            {
+                bottom.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void BottomLeft_Click(object sender, System.EventArgs e)
+        {
+            if (bottomLeft.Text == " ")
+            {
+                bottomLeft.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void MiddleRight_Click(object sender, System.EventArgs e)
+        {
+            if (middleRight.Text == " ")
+            {
+                middleRight.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void Middle_Click(object sender, System.EventArgs e)
+        {
+            if (middle.Text == " ")
+            {
+                middle.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void MiddleLeft_Click(object sender, System.EventArgs e)
+        {
+            if (middleLeft.Text == " ")
+            {
+                middleLeft.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void TopRight_Click(object sender, System.EventArgs e)
+        {
+            if (topRight.Text == " ")
+            {
+                topRight.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
+        }
+
+        private void Top_Click(object sender, System.EventArgs e)
+        {
+            if (top.Text == " ")
+            {
+                top.Text = game.turnNow();
+                Update();
+            }
+            else
+                Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
         }
 
         private void TopLeft_Click(object sender, System.EventArgs e)
@@ -38,8 +134,8 @@ namespace TicTacToeGame
             }
             else
                 Toast.MakeText(Application.Context, "Choose an Empty square!", ToastLength.Short).Show();
-
         }
+
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
@@ -53,12 +149,14 @@ namespace TicTacToeGame
             if (game.IfWin())
             {
                 Toast.MakeText(Application.Context, game.WhoWon(), ToastLength.Short).Show();
-                game.StartOver();
+                resetGame();
             }
             else
+            {
                 game.SwitchTurn();
-
+            }
         }
+
         public void Init()
         {
             topLeft = FindViewById<Button>(Resource.Id.topLeft);
@@ -70,6 +168,21 @@ namespace TicTacToeGame
             bottomLeft = FindViewById<Button>(Resource.Id.bottomLeft);
             bottom = FindViewById<Button>(Resource.Id.bottom);
             bottomRight = FindViewById<Button>(Resource.Id.bottomRight);
+            game = new TicTacToe();
+        }
+
+        public void resetGame()
+        {
+            game.StartOver();
+            topLeft.Text = " ";
+            top.Text = " ";
+            topRight.Text = " ";
+            middleLeft.Text = " ";
+            middle.Text = " ";
+            middleRight.Text = " ";
+            bottomLeft.Text = " ";
+            bottom.Text = " ";
+            bottomRight.Text = " ";
         }
     }
 }
