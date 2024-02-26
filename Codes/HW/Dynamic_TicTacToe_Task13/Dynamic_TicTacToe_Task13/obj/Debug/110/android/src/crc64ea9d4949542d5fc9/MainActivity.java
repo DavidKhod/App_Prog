a794661593d181c75e1f69f7a72ce3d35051c475
David_KH <Davidkh207@gmail.com>
2024-02-27 00:03:21 +0200

    Merging 2

--- Codes/HW/Dynamic_TicTacToe_Task13/Dynamic_TicTacToe_Task13/obj/Debug/110/android/src/crc64ea9d4949542d5fc9/MainActivity.java
+++ Codes/HW/Dynamic_TicTacToe_Task13/Dynamic_TicTacToe_Task13/obj/Debug/110/android/src/crc64ea9d4949542d5fc9/MainActivity.java
@@ -20,16 +20,18 @@ public class MainActivity
 	public MainActivity ()
 	{
 		super ();
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("Dynamic_TicTacToe_Task13.MainActivity, Dynamic_TicTacToe_Task13", "", this, new java.lang.Object[] {  });
+		}
 	}
 
 
 	public MainActivity (int p0)
 	{
 		super (p0);
-		if (getClass () == MainActivity.class)
+		if (getClass () == MainActivity.class) {
 			mono.android.TypeManager.Activate ("Dynamic_TicTacToe_Task13.MainActivity, Dynamic_TicTacToe_Task13", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
 	}
 
 
