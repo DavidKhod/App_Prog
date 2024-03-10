9131e746c09ec3cbced0397351a2f5aaf7972f22
David_KH <Davidkh207@gmail.com>
2024-03-11 00:15:47 +0200

    Made some changes

--- Codes/Projects/App2/App2/MainActivity.cs
+++ Codes/Projects/App2/App2/MainActivity.cs
@@ -1,26 +1,27 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
-using AndroidX.AppCompat.App;
 using Android.Widget;
-using Android.Content;
+using AndroidX.AppCompat.App;
 using System;
 namespace App2
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button btnCloseScreen,btnShowImages,btnClearScreen;
+        Button btnCloseScreen, btnShowImages, btnClearScreen;
         LinearLayout mainLayout, imagesLayout;
         Random rnd;
+        EditText editTextCount;
+        int cnt;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Init();
         }
-
         void Init()
         {
             btnClearScreen = FindViewById<Button>(Resource.Id.btnClearScreen);
@@ -28,6 +29,7 @@ namespace App2
             btnShowImages = FindViewById<Button>(Resource.Id.btnShowImages);
             mainLayout = FindViewById<LinearLayout>(Resource.Id.mainLayout);
             imagesLayout = FindViewById<LinearLayout>(Resource.Id.imagesLayout);
+            editTextCount = FindViewById<EditText>(Resource.Id.editTextCount);
             rnd = new Random();
             btnCloseScreen.Click += BtnCloseScreen_Click;
             btnShowImages.Click += BtnShowImages_Click;
@@ -42,18 +44,59 @@ namespace App2
         private void BtnShowImages_Click(object sender, EventArgs e)
         {
             imagesLayout.RemoveAllViews();
-            for (int i = 0; i < 4; i++)
+            string count = editTextCount.Text;
+            if (string.IsNullOrEmpty(count))
+                cnt = 0;
+            else
+                cnt = int.Parse(count);
+            char[] types = new char[] { 's', 'd', 'h', 'c' };
+            var tempLayout = new LinearLayout(this);
+            for (int i = 0; i <= cnt; i++)
             {
-                int x = rnd.Next(1, 14);
-                char[] types = new char[] { 'S', 'D', 'H', 'C' };
-                char type = types[rnd.Next(types.Length)];
-                string imagename = $"img{x}{type}";
-                int id = Resources.GetIdentifier(imagename, "drawable", this.PackageName);
+                if (i % 5 == 0)
+                {
+                    if (i > 0)
+                    {
+                        var img = new ImageView(this);
+                        img.SetImageResource(Resources.GetIdentifier($"img{rnd.Next(1, 14)}{types[rnd.Next(4)]}", "drawable", this.PackageName));
+                        img.LayoutParameters = new LinearLayout.LayoutParams(0, 200, 1);
+                        tempLayout.AddView(img);
+                    }
+                    imagesLayout.AddView(tempLayout);
+                    tempLayout = new LinearLayout(this);
+                    continue;
+                }
                 var imgv = new ImageView(this);
-                imgv.SetImageResource(id);
-                imgv.LayoutParameters = new LinearLayout.LayoutParams(0,200,1);
-                imagesLayout.AddView(imgv);
+                imgv.SetImageResource(Resources.GetIdentifier($"img{rnd.Next(1, 14)}{types[rnd.Next(4)]}", "drawable", this.PackageName));
+                imgv.LayoutParameters = new LinearLayout.LayoutParams(0, 200, 1);
+                tempLayout.AddView(imgv);
             }
+            imagesLayout.AddView(tempLayout);
+        }
+
+        //void addalayoutwithrandomcards()
+        //{
+
+        //    var layout = new linearlayout(this);
+        //    layout.layoutparameters = new linearlayout.layoutparams(linearlayout.layoutparams.matchparent, linearlayout.layoutparams.wrapcontent, 1);
+        //    layout.orientation = orientation.horizontal;
+        //    for (int i = 0; i < 5 && cnt > 0; i++, cnt--)
+        //    {
+        //        var imgv = new imageview(this);
+        //        imgv.setimageresource(generaterandomcardimageidentifier());
+        //        imgv.layoutparameters = new linearlayout.layoutparams(0, linearlayout.layoutparams.matchparent, 1);
+        //        layout.addview(imgv);
+        //    }
+        //    imageslayout.addview(layout);
+        //}
+
+        int GenerateRandomCardImageIdentifier()
+        {
+            int x = rnd.Next(1, 14);
+            char[] types = new char[] { 's', 'd', 'h', 'c' };
+            char type = types[rnd.Next(types.Length)];
+            string imagename = $"img{x}{type}";
+            return Resources.GetIdentifier(imagename, "drawable", this.PackageName);
         }
 
         private void BtnCloseScreen_Click(object sender, System.EventArgs e)
