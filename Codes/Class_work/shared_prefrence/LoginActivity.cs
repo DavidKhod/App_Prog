e1b55e3b907ebd3b3ccd173cbea95711874d3889
DavidKhod <Davidk207@gmail.com>
2024-10-01 08:32:52 +0300

    Finished task 3, shared prefrence

---
+++ Codes/Class_work/shared_prefrence/LoginActivity.cs
@@ -0,0 +1,90 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace shared_prefrence
+{
+    [Activity(Label = "LoginActivity", MainLauncher = true)]
+    public class LoginActivity : Activity
+    {
+        Button loginBtn, signUpBtn;
+        string username, password;
+        CheckBox saveData;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_login);
+            Init();
+        }
+
+        public void Init()
+        {
+            signUpBtn = FindViewById<Button>(Resource.Id.signUp);
+            loginBtn = FindViewById<Button>(Resource.Id.login);
+            saveData = FindViewById<CheckBox>(Resource.Id.saveData);
+            loginBtn.Click += LoginBtn_Click;
+            signUpBtn.Click += SignUpBtn_Click;
+        }
+
+        private void LoginBtn_Click(object sender, System.EventArgs e)
+        {
+            username = FindViewById<EditText>(Resource.Id.username).Text;
+            password = FindViewById<EditText>(Resource.Id.password).Text;
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                var sp = GetSharedPreferences("userData", FileCreationMode.Private);
+                if (username == sp.GetString("username", "None") && password == sp.GetString("password", "None"))
+                {
+                    var editor = sp.Edit();
+                    editor.PutBoolean("saveDataChecked", saveData.Checked);
+                    editor.Apply();
+                    GoToNextPage();
+                }
+                else
+                {
+                    Toast.MakeText(this, "This account doesn't exist", ToastLength.Short).Show();
+                    Toast.MakeText(this, "Please sign up first", ToastLength.Short).Show();
+                }
+            }
+            else
+            {
+                Toast.MakeText(this, "Please input your username and password", ToastLength.Short).Show();
+            }
+        }
+
+        private void SignUpBtn_Click(object sender, System.EventArgs e)
+        {
+            username = FindViewById<EditText>(Resource.Id.username).Text;
+            password = FindViewById<EditText>(Resource.Id.password).Text;
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                var sp = GetSharedPreferences("userData", FileCreationMode.Private);
+                if (username == sp.GetString("username", "None") && password == sp.GetString("password", "None"))
+                {
+                    Toast.MakeText(this, "This account already exists", ToastLength.Short).Show();
+                }
+                else
+                {
+                    var editor = sp.Edit();
+                    editor.PutString("username", username);
+                    editor.PutString("password", password);
+                    editor.PutBoolean("saveDataChecked", saveData.Checked);
+                    editor.Apply();
+                    GoToNextPage();
+                }
+            }
+            else
+            {
+                Toast.MakeText(this, "Please input your username and password", ToastLength.Short).Show();
+            }
+        }
+
+        private void GoToNextPage()
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
---
+++ Codes/Class_work/shared_prefrence/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace shared_prefrence
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class MainActivity : AppCompatActivity
+    {
+        Button logout;
+        TextView introduction_msg;
+        string username, password;
+        bool saveDataChecked;
+        ISharedPreferences sp;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Init();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void Init()
+        {
+            logout = FindViewById<Button>(Resource.Id.logout);
+            logout.Click += Logout_Click;
+            introduction_msg = FindViewById<TextView>(Resource.Id.introcudtion);
+            sp = GetSharedPreferences("userData", FileCreationMode.Private);
+            username = sp.GetString("username", "No username provided");
+            password = sp.GetString("password", "None");
+            saveDataChecked = sp.GetBoolean("saveDataChecked", false);
+            introduction_msg.Text = $"Hello: {username}";
+        }
+
+        private void Logout_Click(object sender, System.EventArgs e)
+        {
+            if (!saveDataChecked)
+                sp.Dispose();
+            var intent = new Intent(this, typeof(LoginActivity));
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
