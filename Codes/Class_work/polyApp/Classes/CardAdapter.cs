1d4dfd630f299bd7c37ab34d7fe8b09115512cee
DavidKhod <Davidk207@gmail.com>
2024-12-10 13:28:24 +0200

    Almost finished task 8, have duplicates when showing the cards

--- Codes/Class_work/polyApp/Classes/CardAdapter.cs
+++ Codes/Class_work/polyApp/Classes/CardAdapter.cs
@@ -22,18 +22,20 @@ namespace polyApp.Classes
         public override long GetItemId(int position) => position;
         public override View GetView(int position, View convertView, ViewGroup parent)
         {
-            GreetingCard greetingCard = _greetingCards[position];
-            if (convertView == null)
-                convertView = LayoutInflater.From(_context).Inflate(Resource.Layout.item_draft, parent, false);
+            GreetingCard greetingCard = _greetingCards[position];//greeting card picked
+            convertView = convertView ?? LayoutInflater.From(_context).Inflate(Resource.Layout.item_draft, parent, false);
             var cardImage = convertView.FindViewById<ImageView>(Resource.Id.cardImageIV);
+            //sender to recipient textview initialization
             var senderToRecipientTV = convertView.FindViewById<TextView>(Resource.Id.fromToDataTV);
             senderToRecipientTV.LayoutParameters = new LinearLayout.LayoutParams(650, -2);
+            //greeting message textview initialization
             var greetingMessageTV = convertView.FindViewById<TextView>(Resource.Id.greetingMessageTV);
             greetingMessageTV.LayoutParameters = new LinearLayout.LayoutParams(650, -2);
+            //initiating the button that shows the dialog
             var showBtn = convertView.FindViewById<TextView>(Resource.Id.showBtn);
             showBtn.Tag = position;
             showBtn.Click += ShowBtn_Click;
-            cardImage.SetImageResource(greetingCard.imageViewID);
+            cardImage.SetImageResource(greetingCard.imageViewID);//setting the dialog picture to the picture of the picked card type
             cardImage.LayoutParameters = new LinearLayout.LayoutParams(200, 200);
             senderToRecipientTV.Text = greetingCard.ToString();
             greetingMessageTV.Text = greetingCard.GreetingMsg();
@@ -52,7 +54,7 @@ namespace polyApp.Classes
             img.SetImageResource(_greetingCards[tag].imageViewID);
             img.LayoutParameters = new FrameLayout.LayoutParams(-2, -2);
             greetingMessageTV = dialog.FindViewById<TextView>(Resource.Id.greetingMessage);
-            greetingMessageTV.Text = _greetingCards[tag].GreetingMsg();
+            greetingMessageTV.Text = _greetingCards[tag].GreetingMsg();//setting the dialog text to the card picked greeting message
             dialog.Show();
         }
     }
--- Codes/Class_work/polyApp/MainActivity.cs
+++ Codes/Class_work/polyApp/MainActivity.cs
@@ -1,16 +1,32 @@
 ﻿using Android.App;
-using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
+using polyApp.Classes;
+using System;
+using System.Collections.Generic;
 
 namespace polyApp
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button wedCardBtn, birthCardBtn, showAllCardsBtn, toListViewBtn;
+        private TabHost _tabHost;
+        private TabWidget _tabWidget;
+        private FrameLayout _tabContent;
+        const int CREATE_TAB = 0, SHOW_CARDS_TAB = 1;
+        GreetingCards instance = GreetingCards.GetInstance();
+        //Create card attributes
+        RadioGroup optionRadGroup;
+        EditText recipientView, senderView, brideView, ageView;
+        Button addCard;
+        string type;
+        //Show card attributes
+        Spinner spinner;
+        ListView listView;
+        List<GreetingCard> cards;
+        int filterID;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,40 +37,149 @@ namespace polyApp
 
         public void Init()
         {
-            wedCardBtn = FindViewById<Button>(Resource.Id.wedCardBtn);
-            wedCardBtn.Click += WedCardBtn_Click;
-            birthCardBtn = FindViewById<Button>(Resource.Id.birthCardBtn);
-            birthCardBtn.Click += BirthCardBtn_Click;
-            showAllCardsBtn = FindViewById<Button>(Resource.Id.showAllCardBtn);
-            showAllCardsBtn.Click += ShowAllCardsBtn_Click;
-            toListViewBtn = FindViewById<Button>(Resource.Id.toListViewBtn);
-            toListViewBtn.Click += ToListViewBtn_Click;
+            // Initialize TabHost and its components
+            _tabHost = FindViewById<TabHost>(Resource.Id.tabHost);
+            //_tabWidget = FindViewById<TabWidget>(Resource.Id.tabs);
+            //_tabContent = FindViewById<FrameLayout>(Resource.Id.tabContent);
+
+            // Set up the TabHost
+            _tabHost.Setup();
+
+            // Add Tab 1 - "Create Card"
+            TabHost.TabSpec tabSpec1 = _tabHost.NewTabSpec("createCard");
+            tabSpec1.SetIndicator("Create Card"); // Tab name
+            tabSpec1.SetContent(Resource.Id.createCardLay); // Layout resource for the tab
+            _tabHost.AddTab(tabSpec1);
+
+            // Add Tab 2 - "Show Card"
+            TabHost.TabSpec tabSpec2 = _tabHost.NewTabSpec("showCard");
+            tabSpec2.SetIndicator("Show Card"); // Tab name
+            tabSpec2.SetContent(Resource.Id.showCardLay); // Layout resource for the tab
+            _tabHost.AddTab(tabSpec2);
+
+            // Optionally, set the first tab to be selected by default
+            _tabHost.CurrentTab = SHOW_CARDS_TAB;
+            _tabHost.TabChanged += _tabHost_TabChanged;
+            spinner = FindViewById<Spinner>(Resource.Id.optionSpinner);
+            var options = new string[] { "Wedding", "Youth Birthday", "Adult Birthday", "All cards" };
+            var adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, options);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+            spinner.ItemSelected += Spinner_ItemSelected;
         }
 
-        private void ToListViewBtn_Click(object sender, System.EventArgs e)
+        private void Spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
         {
-            Intent intent = new Intent(this, typeof(filterToListView));
-            StartActivity(intent);
+            filterID = e.Position;
+            InitiateShowCardsTab();
         }
 
-        private void ShowAllCardsBtn_Click(object sender, System.EventArgs e)
+        public void InitiateShowCardsTab()
         {
-            Intent intent = new Intent(this, typeof(ShowCards));
-            StartActivity(intent);
+            switch (filterID)
+            {
+                case 0:
+                    cards = instance.Filter<WeddingCard>();
+                    break;
+                case 1:
+                    cards = instance.Filter<YouthBirthdayCard>();
+                    break;
+                case 2:
+                    cards = instance.Filter<AdultBirthdayCard>();
+                    break;
+                default:
+                    cards = null;
+                    break;
+            }
+            CardAdapter cardAdapter = new CardAdapter(this, cards);
+            listView = FindViewById<ListView>(Resource.Id.listView);
+            listView.Adapter = cardAdapter;
+        }
+        private void _tabHost_TabChanged(object sender, TabHost.TabChangeEventArgs e)
+        {
+            if (e.TabId == "createCard")
+            {
+                _tabHost.CurrentTab = CREATE_TAB;
+                InitiateCreateCardTab();
+            }
+            else if (e.TabId == "showCard")
+            {
+                _tabHost.CurrentTab = SHOW_CARDS_TAB;
+                InitiateShowCardsTab();
+            }
         }
 
-        private void BirthCardBtn_Click(object sender, System.EventArgs e)
+        public void UpdateCardCreationByOption()
         {
-            Intent intent = new Intent(this, typeof(CreateCard));
-            intent.PutExtra("type", "Birthday");
-            StartActivity(intent);
+            switch (type.ToLower())
+            {
+                case "birthday":
+                    ageView.LayoutParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, (LinearLayout.LayoutParams.WrapContent));
+                    recipientView.Hint = "Recipient";
+                    brideView.LayoutParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, 0);
+                    break;
+                default:
+                    brideView.LayoutParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, (LinearLayout.LayoutParams.WrapContent));
+                    recipientView.Hint = "Groom";
+                    ageView.LayoutParameters = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MatchParent, 0);
+                    break;
+            }
         }
 
-        private void WedCardBtn_Click(object sender, System.EventArgs e)
+        public void InitiateCreateCardTab()
         {
-            Intent intent = new Intent(this, typeof(CreateCard));
-            intent.PutExtra("type", "Wedding");
-            StartActivity(intent);
+            type = "wedding";
+            optionRadGroup = FindViewById<RadioGroup>(Resource.Id.optionRadGroup);
+            optionRadGroup.CheckedChange += OptionRadGroup_CheckedChange;
+            recipientView = FindViewById<EditText>(Resource.Id.recipient);
+            senderView = FindViewById<EditText>(Resource.Id.sender);
+            brideView = FindViewById<EditText>(Resource.Id.bride);
+            ageView = FindViewById<EditText>(Resource.Id.age);
+            UpdateCardCreationByOption();
+            addCard = FindViewById<Button>(Resource.Id.addCard);
+            addCard.Click += AddCard_Click;
+        }
+
+        private void OptionRadGroup_CheckedChange(object sender, RadioGroup.CheckedChangeEventArgs e)
+        {
+            switch (e.CheckedId)
+            {
+                case Resource.Id.weddingOption:
+                    type = "wedding";
+                    UpdateCardCreationByOption();
+                    break;
+                case Resource.Id.birthdayOption:
+                    type = "birthday";
+                    UpdateCardCreationByOption();
+                    break;
+            }
+        }
+
+        private void AddCard_Click(object sender, EventArgs e)
+        {
+            if (type.ToLower() == "wedding")
+            {
+                string groom = recipientView.Text;
+                string bride = brideView.Text;
+                string senderStr = senderView.Text;
+                instance.Add(new WeddingCard(groom, bride, senderStr));
+            }
+            else
+            {
+                string recipientStr = recipientView.Text;
+                string senderStr = senderView.Text;
+                int age;
+                int.TryParse(ageView.Text, out age);
+                if (age > 18)
+                    instance.Add(new AdultBirthdayCard(recipientStr, senderStr, age));
+                else
+                    instance.Add(new YouthBirthdayCard(recipientStr, senderStr, age));
+            }
+            recipientView.Text = "";
+            brideView.Text = "";
+            ageView.Text = "";
+            senderView.Text = "";
+            _tabHost.CurrentTab = SHOW_CARDS_TAB;
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
